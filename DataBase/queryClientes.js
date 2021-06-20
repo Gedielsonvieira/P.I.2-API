66ffaae5036fda3b70116665e456170e44ea8f6f
Gedielson Vieira <gedielsonvieira@outlook.com>
2021-06-20 12:29:37 -0300

    nova consulta ao bd

--- DataBase/queryClientes.js
+++ DataBase/queryClientes.js
@@ -1,14 +1,10 @@
 let queryClientes = {
     listar : 'SELECT * from cliente',
-    inserir : "INSERT INTO cliente (nome, email, cpf, username, senha) VALUES (?, ?, ?, ?, ?)",//add  (username,senha) VALUES (?,?,?,?)" e no BD TBM
+    inserir : "INSERT INTO cliente (nome, email, cpf, username, senha) VALUES (?, ?, ?, ?, ?)",
     buscaPorId : "SELECT * FROM cliente WHERE id=?",
     atualizar : `UPDATE cliente SET nome=?, email=?, cpf=? WHERE id=?`,
     deletar : `DELETE FROM cliente WHERE id=?`,
-    buscarPorUsername: "SELECT * FROM cliente where username=?"
+    buscarPorUsername: "SELECT * FROM cliente WHERE username=?"
 }
 
 module.exports = queryClientes;
-
-//saber como utilizar e por que req.query - qualquer busca n sendo algo único utiliza-se req.query (listagem, search)
-
-//repository normalmente se comunica com o bd - EU ACHO
