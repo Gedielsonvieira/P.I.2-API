f4e7fcdc0aae613ce817edb6d29af3e2f1744d6c
Gedielson Vieira <gedielsonvieira@outlook.com>
2021-07-11 19:36:10 -0300

    Instalando cors

--- app2.js
+++ app2.js
@@ -1,17 +1,13 @@
 const express = require('express')//Gerencia requisições de diferentes verbos HTTP
+const cors = require('cors')//importando cors
 const app = express()
 const port = 3000
 const clienteController = require('./controller/clienteController')
 
+app.use(cors())//disponibiliza no servidor o acesso a aplicações a trabalharem com a api
 //Para poder trabalhar com JSON precisamos trabalhar com as duas linhas abaixo  
 app.use(express.json())
 app.use(express.urlencoded({ extended: true}))  
-
-let count = 1;
-app.use((req, res, next) => {
-   console.log(`Passando pelo Middleware ${count++}`);
-   next();
-})
 
 //Rota para verificação do login
 const authRota = require('./rotas/authRotas')
@@ -29,7 +25,8 @@ app.use('/api/produtos', atalhoProdutoRota)
 const atalhoClienteRota = require('./rotas/clienteRotas')  
 app.use('/api/clientes', atalhoClienteRota) 
 
-
+/*
+*/ 
 
 app.listen(port, () => {// escuta as requisições vindas da porta definida
    let data = new Date();
